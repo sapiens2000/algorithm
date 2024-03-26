81c24d69d1b8b14e46e3a44c13fb64fb7b9a5b24
ByeongGyu Jeon <cube9928@naver.com>
2024-03-26 20:29:22 +0900

    Update 11053.py

--- dp/11053.py
+++ dp/11053.py
@@ -1,20 +1,12 @@
 n = int(input())
-s = [0]
-d = [[0 for _ in range(2)] for _ in range(n+1)]
+nums = list(map(int, input().split()))
 
-for _ in range(n):
-    s.append(int(input()))
+d = [1 for _ in range(n)]
 
-if n == 1:
-    print(s[1])
-else:
-    d[1][0] = s[1]
-    d[1][1] = 0
-    d[2][0] = s[2]
-    d[2][1] = s[1] + s[2]
 
-    for i in range(3, n+1):
-        d[i][0] = s[i] + max(d[i-2][0], d[i-2][1])
-        d[i][1] = d[i-1][0] + s[i]
+for i in range(n):
+    for j in range(0, i):
+        if nums[j] < nums[i]:
+            d[i] = max(d[i], d[j]+1)
 
-    print(max(d[n][0], d[n][1]))
+print(max(d))
