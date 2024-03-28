9fdeb5f1d9b6998f81241e38633ecb99ecd868ce
ByeongGyu Jeon <cube9928@naver.com>
2024-03-28 21:33:05 +0900

    Create 1991.py

---
+++ tree/1991.py
@@ -0,0 +1,35 @@
+
+n = int(input())
+tree = {}
+
+for _ in range(n):
+    root, left, right = input().split()
+    tree[root] = [left, right]
+
+
+def preorder(cur):
+    if cur != '.':
+        print(cur, end='')
+        preorder(tree[cur][0])
+        preorder(tree[cur][1])
+
+
+def inorder(cur):
+    if cur != '.':
+        inorder(tree[cur][0])
+        print(cur, end='')
+        inorder(tree[cur][1])
+
+
+def postorder(cur):
+    if cur != '.':
+        postorder(tree[cur][0])
+        postorder(tree[cur][1])
+        print(cur, end='')
+
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
