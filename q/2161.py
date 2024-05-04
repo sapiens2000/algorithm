f61d55a837190b2c3336fee5357228dc9e7e0e32
ByeongGyu Jeon <cube9928@naver.com>
2024-05-04 15:36:41 +0900

    Update 2161.py

--- q/2161.py
+++ q/2161.py
@@ -1,18 +1,11 @@
-from collections import deque
-
-
 n = int(input())
-arr = deque([i for i in range(1, n+1)])
+arr = [i for i in range(1, n+1)]
 result = []
 idx = 0
 
-while True:
-    if len(arr) == 1:
-        break
-    head = arr.popleft()
-    result.append(head)
-    arr.append(arr.popleft())
-
+while len(arr) != 1:
+    result.append(arr.pop(0))
+    arr.append(arr.pop(0))
 
 for num in result:
     print(num,end=' ')
