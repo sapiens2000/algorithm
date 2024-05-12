b1b06d51b53bc004ab191fa153416f843d337276
ByeongGyu Jeon <cube9928@naver.com>
2024-05-12 20:13:40 +0900

    Create 4406.py

---
+++ swea/4406.py
@@ -0,0 +1,15 @@
+t = int(input())
+dic = ['a','e','i','o','u']
+
+
+for case in range(1, t+1):
+    arr = input().rstrip()
+    tmp = []
+
+    for c in arr:
+        if c in dic:
+            continue
+        tmp.append(c)
+
+    result = ''.join(tmp)
+    print(f'#{case} {result}')
