ec1fbe55993ee31cf660f014873bbcdfca0cb319
ByeongGyu Jeon <cube9928@naver.com>
2024-05-23 23:54:09 +0900

    Update 2846.py

--- impl/2846.py
+++ impl/2846.py
@@ -1,16 +1,15 @@
 n = int(input())
 arr = list(map(int, input().split()))
-s = []
-
 prev = arr[0]
 diff = 0
 result = 0
+
 for num in arr:
-    if num < prev:
+    if num > prev:
         diff += num - prev
     else:
-        result = max(res, diff)
+        result = max(result, diff)
         diff = 0
-
+    prev = num
 
 print(max(diff, result))
