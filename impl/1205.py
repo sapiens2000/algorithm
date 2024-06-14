5c33bba73e694e2060a81804d66b43273cfd09ad
ByeongGyu Jeon <cube9928@naver.com>
2024-06-14 23:47:14 +0900

    Update 1205.py

--- impl/1205.py
+++ impl/1205.py
@@ -2,16 +2,16 @@ n, score, p = map(int, input().split())
 
 if n:
     nums = list(map(int, input().split()))
-    tmp = -1
 
     if n == p and nums[-1] >= score:
         print(-1)
     else:
+        tmp = n + 1
 
         for i in range(n):
             if nums[i] <= score:
-                print(i + 1)
+                tmp = i + 1
                 break
-
+        print(tmp)
 else:
     print(1)
