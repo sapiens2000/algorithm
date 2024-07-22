ce83943cd7391e6ac6c556f4081356f6d5aeff53
ByeongGyu Jeon <cube9928@naver.com>
2024-07-22 23:45:16 +0900

    Create 20363.java

---
+++ 20363.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        System.out.println((int)(Math.floor(x + y + (Math.min(x, y) / 10))));
+    }
+
+}
